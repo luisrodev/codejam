67893abb873972784d73544fcdae48360e7d7370
Luis A. Rubio <luirting@gmail.com>
2016-07-05 23:21:25 -0700

    Se creo el archiv Solution.java
    El archivo Solution.java del segundo ejercicio fue creado con algunas de las
    variables necesarias para realizar el ejercicio.

---
+++ Qualification Round Africa 2010/B ReverseWords/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution{
+
+  int N; //Numer of cases.
+  int L; //Number of letters.
+  int W; //Number of words.
+
+  public static void main(String[] args){
+    
+  }
+}
