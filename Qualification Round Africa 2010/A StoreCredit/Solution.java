d2d67de48f2aad8ec0dc2c2ad14a64b811f64855
Luis A. Rubio <luirting@gmail.com>
2016-07-07 16:11:43 -0700

    Solution.java file cleaned
    Solution.java file was clean, removing commented lines.

--- Qualification Round Africa 2010/A StoreCredit/Solution.java
+++ Qualification Round Africa 2010/A StoreCredit/Solution.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Solution{
 
   public static void main(String[] args){
+
   	Scanner scan = new Scanner(System.in);
+
   	int N;//Number of cases.
   	int C;//Amount of credit.
   	int L;//Number of items in the store.
@@ -11,30 +13,25 @@ public class Solution{
 
   	N = Integer.parseInt(scan.nextLine());
 
-
-  	//System.out.println("Valor N: " + N);
   	//for each cases in the file.
   	for(int i = 0; i < N; i++){
 	    C = Integer.parseInt(scan.nextLine());
-	    //L = new int[Integer.parseInt(scan.nextLine())];
+
 	    L = Integer.parseInt(scan.nextLine());
-	    //System.out.println("valor L: " + L);
+
 	    P = new int[L];
 	    String cad = scan.nextLine();
-	    //System.out.println("valor cad: " + cad);
+
 	    String[] arrCad = cad.split(" ");
 
 	    for(int j = 0; j < L; j++){
-    		//System.out.println("SOy un item");
-    		//P[j] = Integer.parseInt(scan.nextLine());
     		P[j] = Integer.parseInt(arrCad[j]);
 	    }
 
 	    //print the items on P
-	    //boolean g = false;
-	    for(int p = 0; p < L /*&& g == false*/; p++){
-	        //System.out.println("Value item: " + (p + 1) + " is: " + P[p]);
-    		for(int j = p; j < L /*&& g == false*/; j++){
+	    for(int p = 0; p < L; p++){
+
+    		for(int j = p; j < L; j++){
   		    if(p != j){
   		      if(verifi(C, P[p], P[j])){
   			        System.out.println("Case #" + (i + 1) + ": " + (p + 1) + " " + (j + 1));
